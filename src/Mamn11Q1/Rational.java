04e06c9e9754b6a9dff5fa819cbb9d6f68838f00
ilyaefish <ilyaefish@users.noreply.github.com>
2017-10-21 18:57:49 +0300

    ma
    commit first

---
+++ src/Mamn11Q1/Rational.java
@@ -0,0 +1,96 @@
+package Mamn11Q1;
+
+
+public class Rational {
+
+	private int P;
+	private double Q;
+
+	public Rational(int p, double q) {
+		if (q > 0) {
+			this.P = p;
+			this.Q = q;
+		} else {
+			this.P = 0;
+			this.Q = 1;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return P + "/" + (int)Q;
+	}
+
+	public int getNumerator() {
+		return P;
+	}
+
+	public double getDenominator() {
+		return Q;
+	}
+
+	public void setNumerator(int p) {
+		this.P = p;
+	}
+
+	public void setDenominator(double q) {
+		this.Q = q;
+	}
+
+	public Boolean greaterThan(Rational R) {
+		if (this.P * R.getDenominator() > this.Q * R.getNumerator()) {
+			return true;
+
+		}
+		return false;
+	}
+
+	public Boolean equals(Rational R) {
+		if (this.P * R.getDenominator() == this.Q * R.getNumerator()) {
+			return true;
+
+		}
+		return false;
+	}
+
+	public Rational plus(Rational R) {
+		int p=(int) ((double) this.P * R.getDenominator() + this.Q * R.getNumerator());
+		double q=((double) this.P * R.getDenominator());
+		Rational ans = new Rational(p,q);
+		return ans;
+
+	}
+
+	public Rational minus(Rational R) {
+		
+		int p=(int) ((double) this.P * R.getDenominator() - this.Q * R.getNumerator());
+		double q=((double) this.P * R.getDenominator());
+		Rational ans = new Rational(p,q);
+		return ans;
+	}
+
+	public Rational multiply(Rational R) {
+
+		Rational ans = new Rational(this.P * R.P, this.Q * R.Q);
+		return ans;
+     	}
+
+	private int gdc(int p, double q) {
+		if (p < 0)
+			p = -p;
+		if (q == 0)
+			return p;
+		else
+			return gdc((int) q, p % q);
+
+	}
+
+	public Rational reduce(Rational R) {
+		int gd = gdc(R.P, R.Q);
+		R.setNumerator(R.P / gd);
+		R.setDenominator(R.Q / gd);
+		return R;
+
+	}
+
+}
---
+++ src/Mamn11Q1/UseRationalClass.java
@@ -0,0 +1,37 @@
+package Mamn11Q1;
+
+import java.util.Scanner;
+
+public class UseRationalClass {
+
+	public static void main(String[] args) {
+		double b;
+		double d;
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please Enter the first Numerator");
+		int a = (int) scan.nextInt();
+
+		do {
+			System.out.println("Please Enter the first Denominator(Positive Only)");
+//			scan.next();
+			b = scan.nextDouble();
+		} while (b <= 0);
+		
+		System.out.println("Please Enter the Second Numerator");
+		int c = scan.nextInt();
+
+		do {
+			System.out.println("Please Enter the Second Denominator(Positive Only)");
+//			scan.next();
+			d = scan.nextDouble();
+		} while (d <= 0);
+		
+		Rational First = new Rational(a, b);
+		Rational Second = new Rational(c, d);
+		Rational Third = First.multiply(Second);
+		System.out.println(First + "*" + Second + " = " + Third.reduce(Third));
+
+	}
+
+}
